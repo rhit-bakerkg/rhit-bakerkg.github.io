4f6862aa15672db1bd928166c0626f7ccd3a100d
rhit-bakerkg <bakerkg@rose-hulman.edu>
2025-09-26 21:52:03 -0400

    Implemented basic header and footer for all pages

---
+++ scripts.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", () => {
+    setupNavbar()
+    setupFooter()
+})
+
+function setupNavbar() {
+    let header = document.querySelector("header")
+    header.innerHTML = `<nav>
+        <a href="./index.html">Home</a> |
+        <a href="./resume.html">Resume</a> |
+        <a href="./portfolio.html">Portfolio</a>
+    </nav>`
+}
+
+function setupFooter() {
+    let footer = document.querySelector("footer")
+    footer.innerHTML = `<p>Additional Links:</p>
+    <ul>
+        <li><a href="https://rose-hulman.joinhandshake.com/profiles/5u2guf">Handshake</a></li>
+        <li><a href="https://www.linkedin.com/in/kayla-baker-352656331">LinkedIn</a></li>
+    </ul>`
+}
+
